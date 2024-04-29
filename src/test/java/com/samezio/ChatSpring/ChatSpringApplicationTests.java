cc3dee17e68ade2ce3456fc0b763a59fc3a59660
Sameer Gupta <sameerg2255@gmail.com>
2024-04-29 22:41:07 +0530

    First commit

---
+++ src/test/java/com/samezio/ChatSpring/ChatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samezio.ChatSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
